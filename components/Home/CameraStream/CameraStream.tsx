6088505f02cd328e63cc2f76882eb0180ff8c246
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-07-24 01:48:16 +0300

    commit

--- components/Home/CameraStream/CameraStream.tsx
+++ components/Home/CameraStream/CameraStream.tsx
@@ -2,23 +2,23 @@ import React from "react";
 import { CameraPreview } from "@awesome-cordova-plugins/camera-preview";
 
 const CameraStream: React.FC = () => {
-  /*   let options = {
-    x: 0,
-    y: 0,
+  let options = {
+    x: 400,
+    y: 400,
     width: window.screen.width,
     height: window.screen.height,
     camera: CameraPreview.CAMERA_DIRECTION.BACK,
-    toBack: true,
-    tapPhoto: true,
-    tapFocus: false,
+    toBack: false,
+    tapPhoto: false,
+    tapFocus: true,
     previewDrag: false,
     storeToFile: false,
     disableExifHeaderStripping: false,
   };
 
   CameraPreview.startCamera(options);
-  CameraPreview.show(); */
-  return <div className="w-screen h-screen bg-blue-400"></div>;
+  CameraPreview.show();
+  return <div className="w-screen h-screen bg-sk"></div>;
 };
 
 export default CameraStream;
