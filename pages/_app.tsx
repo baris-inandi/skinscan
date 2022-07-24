81c7f234bc39eceb830a65029fdf836ca68f6a5b
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-07-24 13:23:28 +0300

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import Script from "next/script";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import FirstTime from "../components/FirstTime/FirstTime";
+import { createStore, get, set, clear as clearStore } from "../store/store";
 
 import "tailwindcss/tailwind.css";
 import "@ionic/react/css/core.css";
@@ -16,9 +16,30 @@ import "@ionic/react/css/display.css";
 import "../styles/global.css";
 import "../styles/variables.css";
 
-const isFirstTimeUsing = true;
+const forceWelcomeOnStartup = false;
 
 function App({ Component, pageProps }) {
+  const [showWelcome, setShowWelcome] = useState(false);
+  const [storeAccessed, setStoreAccessed] = useState(false);
+
+  useEffect(() => {
+    const setupStore = async () => {
+      await createStore("__sk_store");
+      if (forceWelcomeOnStartup) {
+        clearStore();
+        setShowWelcome(true);
+        setStoreAccessed(true);
+        return;
+      }
+      const noFirstTimeRaw = await get("noFirstTime");
+      const noFirstTime = noFirstTimeRaw === true ? true : false;
+      set("noFirstTime", true);
+      setShowWelcome(!noFirstTime);
+      setStoreAccessed(true);
+    };
+    setupStore();
+  }, []);
+
   return (
     <>
       <Head>
@@ -27,16 +48,18 @@ function App({ Component, pageProps }) {
           content="width=device-width, initial-scale=1.0, viewport-fit=cover"
         ></meta>
       </Head>
-      <main className="bg-sk-bg text-sk-fg leading-snug select-none">
-        {isFirstTimeUsing ? (
-          <>
-            <FirstTime />
-            <Component {...pageProps} />
-          </>
-        ) : (
-          <Component {...pageProps} />
-        )}
-      </main>
+      {storeAccessed && (
+        <>
+          {showWelcome !== null && (
+            <main className="bg-sk-bg text-sk-fg leading-snug select-none">
+              <>
+                {showWelcome && <FirstTime />}
+                <Component {...pageProps} />
+              </>
+            </main>
+          )}
+        </>
+      )}
     </>
   );
 }
