e84d1c5c91eb6ad51460213d082596d9df8374c1
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-07-24 16:46:09 +0300

    commit

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,19 @@
 // TODO: @yigitkeremoktay
 
+import { createStore, set } from "../lib/store/store";
+
 export const login = async (email: string, password: string) => {
+  await createStore("__sk_store");
+  set("isAuthenticated", true);
+  // ^^^ DO THIS ONLY IF SUCCESSFULLY AUTHENTICATED
   console.log(email, password, "login");
   return;
 };
 
 export const signup = async (email: string, password: string) => {
+  await createStore("__sk_store");
+  set("isAuthenticated", true);
+  // ^^^ DO THIS ONLY IF SUCCESSFULLY AUTHENTICATED
   console.log(email, password, "signup");
   return;
 };
