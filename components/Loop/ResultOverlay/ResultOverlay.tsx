9d08a2e9f383d386d50e894472e62343d7812e57
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-07-25 03:32:18 +0300

    commit

--- components/Loop/ResultOverlay/ResultOverlay.tsx
+++ components/Loop/ResultOverlay/ResultOverlay.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import { get } from "../../../lib/store/store";
 import { logout } from "../../../lib/auth";
 
@@ -6,28 +6,26 @@ interface Props {
   id: string;
 }
 
-// TODO:
-/* 
-All content should be useState()
-should be display:hidden when not resolved
-it should be rendered as an overlay, coming from the bottom to the top after all fetches are resolved
-*/
-
 export const ResultOverlay: React.FC<Props> = (props) => {
-  console.log(props)
+  console.log(props);
 
   const [isCompleted, setIsComplete] = useState(false);
   const [isLooping, setIsLooping] = useState(false);
 
-  useEffect(function(){
-    if(props.id == 'undefined' || isLooping){
-      return
-    }
-    setIsLooping(true)
-    let interval = setInterval(async function(){
-      const res = await fetch(`https://skinscan.withskyfallen.com/status/${props.id}?token=${await get("currentUserToken")}`);
-      let rspTxt = await res.text();
-      /*if (rspTxt === "Internal Server Error") {
+  useEffect(
+    function () {
+      if (props.id == "undefined" || isLooping) {
+        return;
+      }
+      setIsLooping(true);
+      let interval = setInterval(async function () {
+        const res = await fetch(
+          `https://skinscan.withskyfallen.com/status/${
+            props.id
+          }?token=${await get("currentUserToken")}`
+        );
+        let rspTxt = await res.text();
+        /*if (rspTxt === "Internal Server Error") {
         logout();
       } else {
         const status = String(JSON.parse(rspTxt).status);
@@ -37,8 +35,10 @@ export const ResultOverlay: React.FC<Props> = (props) => {
           console.log("processed")
         }
       }*/
-    }, 1000)
-  }, [props])
+      }, 5000);
+    },
+    [props, isLooping]
+  );
 
   return <div>{props.id}</div>;
 };
